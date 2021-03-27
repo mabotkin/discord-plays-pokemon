59db9ea69c15eeeb8435fe6e8669c9914b0dd604
Alex Wang <alex3763@gmail.com>
2021-03-27 05:43:02 -0400

    takes discord inputs

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-
-require("dotenv").config();
-
+var Discord = require('discord.js');
 var GameBoyAdvance = require('gbajs');
 
+require("dotenv").config();
+var DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+var DISCORD_GUILD_ID = parseInt( process.env.DISCORD_GUILD_ID );
+var DISCORD_CHANNEL_ID = parseInt( process.env.DISCORD_CHANNEL_ID );
 var FRAMERATE = Math.min(Math.max( parseInt( process.env.FRAMERATE ), 1 ), 60 );
 var PORT = parseInt( process.env.PORT );
 var ROMNAME = process.env.ROM_NAME;
@@ -52,4 +54,35 @@ io.on('connection', (socket) => {
 
 http.listen(PORT , function () {
 	console.log('Discord Plays Pokemon running on port ' + PORT + '.');
+});
+
+var client = new Discord.Client();
+client.once('ready', () => {
+	console.log('Discord connection ready!');
+});
+client.login( DISCORD_TOKEN );
+
+var keypad = gba.keypad;
+var legal_buttons = {
+	"A" : keypad.A,
+	"B" : keypad.B,
+	"SELECT" : keypad.SELECT,
+	"START" : keypad.START,
+	"RIGHT" : keypad.RIGHT,
+	"LEFT" : keypad.LEFT,
+	"UP" : keypad.UP,
+	"DOWN" : keypad.DOWN,
+	"R" : keypad.R,
+	"L" : keypad.L
+}
+
+client.on('message', message => {
+	if ( message.guild == DISCORD_GUILD_ID && message.channel == DISCORD_CHANNEL_ID )
+	{
+		var m = message.content.trim();
+		if ( m in legal_buttons ) {
+			keypad.press( legal_buttons[ m ] );
+		}
+		//console.log( message.author.username + ": " + message.content );
+	}
 });
