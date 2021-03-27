435ab4c59443f517693debbe97fab99ddf990414
Alex Wang <alex3763@gmail.com>
2021-03-26 21:57:44 -0400

    emulator runs

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 var express = require('express');
-var app = express();
+var fs = require('fs');
+var GameBoyAdvance = require('gbajs');
+ 
+var gba = new GameBoyAdvance();
+ 
+gba.logLevel = gba.LOG_ERROR;
+ 
+var biosBuf = fs.readFileSync('./node_modules/gbajs/resources/bios.bin');
+gba.setBios(biosBuf);
+gba.setCanvasMemory();
+ 
+gba.loadRomFromFile('roms/pokemon_firered.gba', function (err, result) {
+	if (err) {
+		console.error('loadRom failed:', err);
+		process.exit(1);
+	}
+	//gba.loadSavedataFromFile('/path/to/game.sav');
+	gba.runStable();
+});
+ 
+ /*
+var idx = 0;
+setInterval(function () {
+	var keypad = gba.keypad;
+	keypad.press(keypad.A);
 
+	setTimeout(function () {
+		var png = gba.screenshot();
+		png.pack().pipe(fs.createWriteStream('screenshots/gba' + idx + '.png'));
+		idx++;
+	}, 200);
+}, 2000);
+*/
+
+var app = express();
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
