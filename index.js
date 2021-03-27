256c5dfdbbe2c776850e669b981c3d6cda7469a4
Alex Wang <alex3763@gmail.com>
2021-03-26 22:39:53 -0400

    preparing for socket testing

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var fs = require('fs');
 var GameBoyAdvance = require('gbajs');
+const { createCanvas } = require('canvas');
  
 var gba = new GameBoyAdvance();
  
 gba.logLevel = gba.LOG_ERROR;
  
+var screen = createCanvas( 240 , 160 );
 var biosBuf = fs.readFileSync('./node_modules/gbajs/resources/bios.bin');
 gba.setBios(biosBuf);
-gba.setCanvasMemory();
+gba.setCanvasDirect(screen);
  
 gba.loadRomFromFile('roms/pokemon_firered.gba', function (err, result) {
 	if (err) {
